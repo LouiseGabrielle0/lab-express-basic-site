24d898effaa21ccb1c572d75fd8e756a880ff68a
Louise <louisegabriellecc@gmail.com>
2022-04-19 16:22:49 +0100

    temp layout added

--- app.js
+++ app.js
@@ -3,21 +3,17 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-
+res.sendFile(__dirname + '/views/home.html')
 })
 app.get("/about", (req, res) => {
-
+res.sendFile(__dirname + '/views/about.html')
 })
 app.get("/features", (req, res) => {
-
+res.sendFile(__dirname + '/views/features.html')
 })
 app.get("/gallery", (req, res) => {
-
+res.sendFile(__dirname + '/views/gallery.html')
 })
-
-
-
-
 
 
 app.listen(3000, () => {
